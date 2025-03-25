06b75dd2636101dc1881890cb3c88624008aface
inkcherry <mingzhi.liu@intel.com>
2025-03-25 08:53:42 +0000

    update

--- python/sglang/srt/models/llama.py
+++ python/sglang/srt/models/llama.py
@@ -51,9 +51,11 @@ from sglang.srt.model_loader.weight_utils import (
 )
 from sglang.srt.utils import add_prefix, make_layers
 from sglang.utils import get_exception_traceback
-
+from sglang.srt.distributed import tensor_model_parallel_all_reduce
 logger = logging.getLogger(__name__)
 
+
+TP_OVERLAP=True
 
 class LlamaMLP(nn.Module):
     def __init__(
@@ -86,7 +88,7 @@ class LlamaMLP(nn.Module):
             intermediate_size,
             hidden_size,
             bias=False,
-            reduce_results=True,
+            reduce_results=not TP_OVERLAP,
             quant_config=quant_config,
             prefix=add_prefix("down_proj", prefix),
         )
@@ -165,7 +167,7 @@ class LlamaAttention(nn.Module):
             self.total_num_heads * self.head_dim,
             hidden_size,
             bias=bias,
-            reduce_results=True
+            reduce_results=not TP_OVERLAP,
             quant_config=quant_config,
             prefix=add_prefix("o_proj", prefix),
         )
@@ -269,40 +271,50 @@ class LlamaDecoderLayer(nn.Module):
         residual: Optional[torch.Tensor],
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         
-        # if True:
+        if not TP_OVERLAP:
         
-        # Self Attention
-        if residual is None:
-            residual = hidden_states
-            hidden_states = self.input_layernorm(hidden_states)
+            # Self Attention
+            if residual is None:
+                residual = hidden_states
+                hidden_states = self.input_layernorm(hidden_states)
+            else:
+                hidden_states, residual = self.input_layernorm(hidden_states, residual)
+                hidden_states = self.self_attn(
+                    positions=positions,
+                    hidden_states=hidden_states,
+                    forward_batch=forward_batch,
+                )
+
+                # Fully Connected
+                hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
+                hidden_states = self.mlp(hidden_states)
         else:
-            hidden_states, residual = self.input_layernorm(hidden_states, residual)
-        hidden_states = self.self_attn(
-            positions=positions,
-            hidden_states=hidden_states,
-            forward_batch=forward_batch,
-        )
+            #TP_OVERLAP
+            # Self Attention
 
-        # Fully Connected
-        hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
-        hidden_states = self.mlp(hidden_states)
-        # else:
-        #     if residual is None:
-        #         residual = hidden_states
-        #         hidden_states = self.input_layernorm(hidden_states)
-        #     else:
-        #         hidden_states, residual = self.input_layernorm(hidden_states, residual)
+            if residual is None:
+                residual = hidden_states
+                hidden_states = self.input_layernorm(hidden_states)
+            else:
+                hidden_states, residual = self.input_layernorm(hidden_states, residual)
             
-        #     qkv, _ = self.qkv_proj(hidden_states)
-        #     q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size], dim=-1)
-        #     q, k = self.rotary_emb(positions, q, k)
-        #     attn_output = self.attn(q, k, v, forward_batch)
-        #     output, _ = self.o_proj(attn_output)
-        #     hidden_states=output
+            #attn
+            qkv, _ = self.self_attn.qkv_proj(hidden_states)
+            q, k, v = qkv.split([self.self_attn.q_size, self.self_attn.kv_size, self.self_attn.kv_size], dim=-1)
             
-        #     hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
-        #     hidden_states = self.mlp(hidden_states)
+            q, k = self.self_attn.rotary_emb(positions, q, k)
+            attn_output = self.self_attn.attn(q, k, v, forward_batch)
+            
+            
+            output, _ = self.self_attn.o_proj(attn_output)
+            tensor_model_parallel_all_reduce(output)
+            hidden_states=output
+            # Fully Connected
 
+            hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
+            
+            hidden_states = self.mlp(hidden_states)
+            tensor_model_parallel_all_reduce(hidden_states)
         
         
         return hidden_states, residual
@@ -355,6 +367,8 @@ class LlamaModel(nn.Module):
                 forward_batch,
                 residual,
             )
+        
+        
         hidden_states, _ = self.norm(hidden_states, residual)
         return hidden_states
 
