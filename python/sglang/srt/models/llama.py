6456ac7fa1dc20f21c7c60e49cb3a33f69c15728
inkcherry <mingzhi.liu@intel.com>
2025-03-27 09:35:59 +0000

    fix mmlu for tp_overlap false

--- python/sglang/srt/models/llama.py
+++ python/sglang/srt/models/llama.py
@@ -283,15 +283,15 @@ class LlamaDecoderLayer(nn.Module):
                 hidden_states = self.input_layernorm(hidden_states)
             else:
                 hidden_states, residual = self.input_layernorm(hidden_states, residual)
-                hidden_states = self.self_attn(
-                    positions=positions,
-                    hidden_states=hidden_states,
-                    forward_batch=forward_batch,
-                )
+            hidden_states = self.self_attn(
+                positions=positions,
+                hidden_states=hidden_states,
+                forward_batch=forward_batch,
+            )
 
-                # Fully Connected
-                hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
-                hidden_states = self.mlp(hidden_states)
+            # Fully Connected
+            hidden_states, residual = self.post_attention_layernorm(hidden_states, residual)
+            hidden_states = self.mlp(hidden_states)
                 
             return hidden_states, residual
 
